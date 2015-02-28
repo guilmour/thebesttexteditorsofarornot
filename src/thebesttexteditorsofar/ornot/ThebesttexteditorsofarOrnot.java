91a5a7c7e1211f774a1fe0283200fafc77370c1f
guilmour <guilmour@guilst.com>
2015-02-28 20:40:52 -0300

    uplaoding files

---
+++ src/thebesttexteditorsofar/ornot/ThebesttexteditorsofarOrnot.java
@@ -0,0 +1,20 @@
+
+package thebesttexteditorsofar.ornot;
+import java.util.*;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class ThebesttexteditorsofarOrnot {
+
+
+    public static void main(String[] args) {
+        
+      
+      Janela janela = new Janela();
+      janela.setLocation(200, 200);
+      janela.setIconImage(Toolkit.getDefaultToolkit().getImage("ico.png"));
+      janela.setVisible(true);
+      
+    }
+    
+}
